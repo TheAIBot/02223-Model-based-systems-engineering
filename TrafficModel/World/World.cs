97a9c604d92e84800589e4b251ab6411d5ffe292
Piotr Ładoński <piolado@gmail.com>
2020-09-22 14:47:00 +0200

    Add World and WorldObject

---
+++ TrafficModel/World/World.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrafficModel.World
+{
+    public static class World
+    {
+        private static readonly List<WorldObject> GameObjects = new List<WorldObject>();
+        private static readonly Stack<WorldObject> Trash = new Stack<WorldObject>();
+        private static readonly Stack<WorldObject> NewObjects = new Stack<WorldObject>();
+
+        public static void UpdateWorld()
+        {
+            AddObjects();
+            UpdateObjects();
+            RemoveObjects();
+        }
+
+        private static void RemoveObjects()
+        {
+            while (Trash.Any())
+            {
+                try
+                {
+                    GameObjects.Remove(Trash.Pop());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception while removing worldObject: {e.Message}");
+                    Console.WriteLine($"Stack trace: {e.StackTrace}");
+                }
+            }
+        }
+
+        private static void UpdateObjects()
+        {
+            for (var i = GameObjects.Count - 1; i >= 0; i--)
+            {
+                if (GameObjects[i].Enabled == false)
+                {
+                    continue;
+                }
+
+                GameObjects[i].Update();
+            }
+        }
+
+        private static void AddObjects()
+        {
+            while (NewObjects.Any())
+            {
+                GameObjects.Add(NewObjects.Pop());
+            }
+        }
+
+        public static void AddObject(WorldObject worldObject)
+        {
+            if (GameObjects.Contains(worldObject) || NewObjects.Contains(worldObject))
+            {
+                Console.WriteLine($"Attempt to add existing/queued object with id={worldObject.Id}. Ignoring.");
+            }
+
+            NewObjects.Push(worldObject);
+        }
+
+        public static void DestroyObject(WorldObject worldObject)
+        {
+            Trash.Push(worldObject);
+        }
+    }
+}
---
+++ TrafficModel/World/WorldObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TrafficModel.World
+{
+    public abstract class WorldObject
+    {
+        public Guid Id { get; }
+        public string Name { get; set; }
+        public bool Enabled { get; set; } = true;
+
+        protected WorldObject()
+        {
+            Id = Guid.NewGuid();
+            World.AddObject(this); // It may be removed, but then every object must be added to World after initialization
+        }
+
+        public virtual void Update()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+
+            var other = (WorldObject) obj;
+            return other.Id.Equals(Id);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}({Id})";
+        }
+
+        protected bool Equals(WorldObject other)
+        {
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
