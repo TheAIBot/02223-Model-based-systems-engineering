aee1beced7a7e3321a456804b617e88f8f374b01
TheAIBot <andreasuranus@hotmail.com>
2020-11-11 22:27:10 +0100

    added checks to verify that process commands executed successfully

--- sumo/routeGen.py
+++ sumo/routeGen.py
@@ -16,7 +16,10 @@ def genRandomTrips(mapFilepath, routeIndex, vehicleCount, throughputMultiplier):
     mapFolderPath = os.path.dirname(mapFilepath)
     tripFilepath = os.path.join(mapFolderPath, "trips" + str(routeIndex) + ".xml")
     randomTripsPath = os.path.join(os.environ["SUMO_HOME"], "tools", "randomTrips.py")
-    subprocess.run(["python", randomTripsPath, "-n", mapFilepath, "--begin=" + str(tripsStartTime), "--end=" + str(tripsEndTime), "--period=" + "{0:.1f}".format(arrivalRate), "--binomial=" + str(int(max(1, throughputMultiplier))), "--seed=" + "56", "--fringe-factor=50", "-o",  tripFilepath])
+    procRes = subprocess.run(["python", randomTripsPath, "-n", mapFilepath, "--begin=" + str(tripsStartTime), "--end=" + str(tripsEndTime), "--period=" + "{0:.1f}".format(arrivalRate), "--binomial=" + str(int(max(1, throughputMultiplier))), "--seed=" + "56", "--fringe-factor=50", "-o",  tripFilepath])
+    if procRes.returncode != 0:
+        raise Exception("Executing randomTrips.py failed.")
+    
     return tripFilepath
 
 
@@ -43,7 +46,9 @@ def genRoutesFromTrips(mapFilepath, tripFiles):
     routeFilepath = os.path.join(mapFolderPath, "routes.xml")
     vehicleTypePath = "vehicleTypes/vehicle_type.add.xml"
     routerPath = os.path.join(os.environ["SUMO_HOME"], "bin", "duarouter")
-    subprocess.run([routerPath, "-n", mapFilepath, "--route-files", ",".join(tripFiles), "--no-step-log", "--seed", "56",  "--additional-files", vehicleTypePath, "-o", routeFilepath, "--ignore-errors", "true", "--no-warnings", "true"])
+    procRes = subprocess.run([routerPath, "-n", mapFilepath, "--route-files", ",".join(tripFiles), "--no-step-log", "--seed", "56",  "--additional-files", vehicleTypePath, "-o", routeFilepath, "--ignore-errors", "true", "--no-warnings", "true"])
+    if procRes.returncode != 0:
+        raise Exception("Executing duarouter failed.")
     return routeFilepath
 
 def generateRoutes(mapFilepath, carsPerGen, genCount, throughputMultiplier):
