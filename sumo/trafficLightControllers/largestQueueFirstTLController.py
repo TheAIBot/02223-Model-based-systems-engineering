285c88b0216101dfbd43bd52cbcc6e6d08904ead
TheAIBot <andreasuranus@hotmail.com>
2020-11-17 01:02:52 +0100

    update lqf to reflect changes to magic controller

--- sumo/trafficLightControllers/largestQueueFirstTLController.py
+++ sumo/trafficLightControllers/largestQueueFirstTLController.py
@@ -6,16 +6,25 @@ class ctrl(TrafficLightController):
         super().__init__("Largest queue first")
 
     def updateLights(self, sim, ticks):
-        for tlIntersection in self.tlIntersections:
+        for tlInter in self.tlIntersections:
+            if tlInter.hasTarget():
+                continue
+
             longestQueue = 0
             longestQueueGroup = None
-            for group in tlIntersection.getTrafficLightGroups():
-                queueLength = 0
-                for laneDetectorValue in group.getLaneDetectorValues():
-                    queueLength = max(queueLength, laneDetectorValue)
+            for groupIdx, group in enumerate(tlInter.getTrafficLightGroups()):
+                if tlInter.inGroupsGreenPhase(group) and tlInter.getTimeInCurrentPhase() > 50:
+                    continue
+                queueLength = group.getSumLaneDetectorValues()
                 
                 if queueLength > longestQueue or longestQueueGroup is None:
                     longestQueue = queueLength
                     longestQueueGroup = group
 
-            tlIntersection.setGroupAsGreen(longestQueueGroup, sim)
+            if longestQueueGroup is not None:
+                #allow the previous targetted group to have a green
+                #phase that lasts for atleast 10 steps
+                if tlInter.isInPrevTargetPhase() and tlInter.getTimeInCurrentPhase() < 10 and not tlInter.inGroupsGreenPhase(longestQueueGroup):
+                    continue
+
+                tlInter.setGroupAsGreen(longestQueueGroup, sim)
